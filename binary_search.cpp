c30283543e9c271b09a4c59d12978d560b9a22cc
Nitin-vohra92 <38051493+Nitin-vohra92@users.noreply.github.com>
2019-04-18 19:08:37 +0530

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -21,14 +21,17 @@ bool BinarySearch(int arr[], int n, int k)
     return false;
 }
 
-int main(){
+int main()
+{
     int n;
     cin>>n;
     int arr[n];
     cout<<"Enter The elements of the array"<<endl;
-    for (int i=0;i<n;i++) cin>>arr[i];
+    for (int i=0;i<n;i++)
+        cin>>arr[i];
     cout<<"Enter the key value you want to search"<<endl;
-    int k; cin>>k;
+    int k;
+    cin>>k;
     if (BinarySearch(arr,n,k))
         cout<<"Element is found"<<endl;
 }
